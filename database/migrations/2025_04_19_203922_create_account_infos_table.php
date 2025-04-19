4ed30643777bd3d156c6ede76d883cd999526ad3
ChanAlex2357 <jacqueschanalex@gmail.com>
2025-04-19 23:40:03 +0300

    generate cash models and migrations utils

---
+++ database/migrations/2025_04_19_203922_create_account_infos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccountInfosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('account_infos', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('account_infos');
+    }
+}
